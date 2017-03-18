03aa0af30d2447d34c34e93e3398fccc520f387a
Maxime Ransan <maxime.ransan@gmail.com>
2017-03-18 15:20:20 +0000

    Add recursive type example

--- src/conversion_test.ml
+++ src/conversion_test.ml
@@ -16,3 +16,32 @@ let () =
     "desired": "F", 
     "temperature": { "u" : "C", "v": 0 }
   }|}
+
+
+(** This is an example how to create a Tree in OCaml 
+    and serialize it in JSON *)
+
+let make_node ?(left = Empty) ?(right = Empty) value= Node {
+  value; 
+  left = Some left; 
+  right = Some right;
+}
+
+let () = 
+  let tree = 
+    make_node 
+      ~left:(make_node 
+        ~left:(make_node "2") 
+        ~right:(make_node "3") 
+        "4"
+      )
+      ~right:(make_node 
+        ~left:(make_node "5")
+        ~right:(make_node "6") 
+        "7"
+      )
+      "8"
+  in 
+  let encoder = Pbrt_bsjson.Encoder.empty() in
+  Conversion.MessageEncoder.encode_binary_tree tree encoder;
+  Pbrt_bsjson.Encoder.to_string encoder |> print_endline 
--- src/messages_pb.mli
+++ src/messages_pb.mli
@@ -21,6 +21,16 @@ type response =
   | Error of string
   | Temperature of temperature
 
+type binary_tree_node = {
+  value : string;
+  left : binary_tree option;
+  right : binary_tree option;
+}
+
+and binary_tree =
+  | Node of binary_tree_node
+  | Empty
+
 
 (** {2 Default values} *)
 
@@ -44,6 +54,17 @@ val default_request :
 val default_response : unit -> response
 (** [default_response ()] is the default value for type [response] *)
 
+val default_binary_tree_node : 
+  ?value:string ->
+  ?left:binary_tree option ->
+  ?right:binary_tree option ->
+  unit ->
+  binary_tree_node
+(** [default_binary_tree_node ()] is the default value for type [binary_tree_node] *)
+
+val default_binary_tree : unit -> binary_tree
+(** [default_binary_tree ()] is the default value for type [binary_tree] *)
+
 module Make_decoder(Decoder:Pbrt_json.Decoder_sig) : sig
   
   (** {2 JSON Decoding} *)
@@ -59,6 +80,12 @@ module Make_decoder(Decoder:Pbrt_json.Decoder_sig) : sig
   
   val decode_response : Decoder.t -> response
   (** [decode_response decoder] decodes a [response] value from [decoder] *)
+  
+  val decode_binary_tree_node : Decoder.t -> binary_tree_node
+  (** [decode_binary_tree_node decoder] decodes a [binary_tree_node] value from [decoder] *)
+  
+  val decode_binary_tree : Decoder.t -> binary_tree
+  (** [decode_binary_tree decoder] decodes a [binary_tree] value from [decoder] *)
   
 end
 module Make_encoder(Encoder:Pbrt_json.Encoder_sig) : sig
@@ -76,5 +103,11 @@ module Make_encoder(Encoder:Pbrt_json.Encoder_sig) : sig
   
   val encode_response : response -> Encoder.t -> unit
   (** [encode_response v encoder] encodes [v] with the given [encoder] *)
+  
+  val encode_binary_tree_node : binary_tree_node -> Encoder.t -> unit
+  (** [encode_binary_tree_node v encoder] encodes [v] with the given [encoder] *)
+  
+  val encode_binary_tree : binary_tree -> Encoder.t -> unit
+  (** [encode_binary_tree v encoder] encodes [v] with the given [encoder] *)
   
 end
